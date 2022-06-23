83f7d8d54c494d0afcffeb1e8ff7e65c08f0fcb4
Jesus Gonzalez <jesus@jesusmg.net>
2022-06-23 01:28:55 -0400

    Setup Emacs evil-mode to auto-install.

--- emacsconfig/.config/emacs/init.el
+++ emacsconfig/.config/emacs/init.el
@@ -1,7 +1,12 @@
+;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(package-refresh-contents)
 
+;; Evil mode
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
 (require 'evil)
 (evil-mode 1)
 
