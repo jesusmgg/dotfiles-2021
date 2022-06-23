32c828b2c1b583815fb420c4424236f33a6e9e2a
Jesus Gonzalez <jesus@jesusmg.net>
2022-06-23 03:05:29 -0400

    Update Emacs font settings.

--- emacsconfig/.config/emacs/init.el
+++ emacsconfig/.config/emacs/init.el
@@ -1,3 +1,8 @@
+;; Font
+(set-face-attribute 'default t :font "Comic Shanns Mono-10.5" )
+(set-face-attribute 'default nil :font "Comic Shanns Mono-10" )
+(setq-default line-spacing 0.45)
+
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -9,4 +14,3 @@
   (package-install 'evil))
 (require 'evil)
 (evil-mode 1)
-
