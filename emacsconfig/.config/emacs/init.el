20035d2ad727d01b4a9d0e2b819a359fa894e805
Jesus Gonzalez <jesus@jesusmg.net>
2022-06-24 02:29:19 -0400

    Update Emacs config file.

--- emacsconfig/.config/emacs/init.el
+++ emacsconfig/.config/emacs/init.el
@@ -26,6 +26,11 @@
 (require 'company)
 (company-mode 1)
 
+;; Magit
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+(require 'magit)
+
 ;; Treemacs
 (unless (package-installed-p 'treemacs)
   (package-install 'treemacs))
@@ -33,16 +38,15 @@
 (unless (package-installed-p 'treemacs-evil)
   (package-install 'treemacs-evil))
 (require 'treemacs-evil)
+(unless (package-installed-p 'treemacs-magit)
+  (package-install 'treemacs-magit))
+(require 'treemacs-magit)
+(treemacs-git-mode 'deferred)
+(treemacs-filewatch-mode 1)
 (global-set-key (kbd "C-1") 'treemacs-select-window)
 (global-set-key (kbd "C-x t 1") 'treemacs-delete-other-windows)
 (global-set-key (kbd "C-x t t") 'treemacs)
 (global-set-key (kbd "C-x t d") 'treemacs-select-directory)
-(with-eval-after-load 'treemacs)
-
-;; Magit
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
-(require 'magit)
 
 ;; evil collection
 (unless (package-installed-p 'evil-collection)
@@ -57,7 +61,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit on-screen good-scroll evil)))
+ '(package-selected-packages '(treemacs-magit magit on-screen good-scroll evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
