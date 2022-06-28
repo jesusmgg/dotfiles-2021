8ea02efb2d2946ca54da8a819cf121e21da38fcd
Jesus Gonzalez <jesus@jesusmg.net>
2022-06-27 20:10:34 -0400

    Set up variable font sizes for org-mode.

--- emacsconfig/.config/emacs/init.el
+++ emacsconfig/.config/emacs/init.el
@@ -172,6 +172,30 @@
 (add-hook 'org-mode-hook 'org-appear-mode)
 (setq org-superstar-special-todo-items t)
 (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+(add-hook 'org-mode-hook 'org-mode-extra-settings)
+
+(defun org-mode-extra-settings ()
+  (interactive)
+  (let* ((variable-tuple
+          (cond ((x-list-fonts "Lato")	          '(:font "Lato"))
+                ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                (nil (warn "Cannot find a Sans Serif Font."))))
+         (base-font-color     (face-foreground 'default nil 'default))
+         (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+    (custom-theme-set-faces
+     'user
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.35))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+)
 
 ;; Keybindings
 (global-set-key (kbd "C-x w c") 'visual-fill-column-mode)
