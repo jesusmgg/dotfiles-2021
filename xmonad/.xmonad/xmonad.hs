90bce9c218e78c33a913d21d91bd94c9ef01fbf4
Jesus Gonzalez <jesus@jesusmg.net>
2022-07-06 10:36:32 -0400

    Add mouse resize (WIP) to Xmonad.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,12 +1,17 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Actions.MouseResize
+import XMonad.Layout.WindowArranger
+
+layout = mouseResize $ windowArrange $ layoutHook def
 
 main :: IO()
 main = xmonad $ def 
     { modMask = mod4Mask
     , terminal = "urxvt"
     , focusFollowsMouse = False
+    , layoutHook = layout
     }
     `additionalKeysP`
     [ ("M-f", spawn "firefox")
