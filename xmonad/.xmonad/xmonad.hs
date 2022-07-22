17c3766e326a80a276ad47f1dc09c2b4ba164105
Jesus Gonzalez <jesus@jesusmg.net>
2022-07-21 21:12:12 -0400

    Add XMonad multimonitor and mouse resize support.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,17 +1,22 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
-import XMonad.Actions.MouseResize
 import XMonad.Layout.WindowArranger
+import XMonad.Layout.ResizableTile -- for resizeable tall layout
+import XMonad.Layout.MouseResizableTile -- for mouse control
+import XMonad.Layout.ThreeColumns -- for three column layout
+import XMonad.Layout.Grid -- for additional grid layout
+import XMonad.Layout.NoBorders -- for fullscreen without borders
+import XMonad.Layout.Fullscreen -- fullscreen mode 
 
-layout = mouseResize $ windowArrange $ layoutHook def
+myLayout = smartBorders (ResizableTall 1 (3/100) (1/2) [] ||| Mirror (ResizableTall 1 (3/100) (3/4) []) ||| Grid ||| ThreeColMid 1 (3/100) (1/2) ||| noBorders Full ||| mouseResizableTile) 
 
 main :: IO()
 main = xmonad $ def 
     { modMask = mod4Mask
     , terminal = "urxvt"
     , focusFollowsMouse = False
-    , layoutHook = layout
+    , layoutHook = myLayout
     }
     `additionalKeysP`
     [ ("M-f", spawn "firefox")
