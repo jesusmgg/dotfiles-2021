98429880165138822bbd1cb1798a85d3864854ad
Jesus Gonzalez <jesus@jesusmg.net>
2022-07-30 14:12:58 -0400

    [Emacs] Define minor mode to override keys from other modes.

--- emacsconfig/.config/emacs/init.el
+++ emacsconfig/.config/emacs/init.el
@@ -18,8 +18,21 @@
 
 
 ;; Keybindings
-(global-set-key (kbd "C-x w c") 'visual-fill-column-mode)
-(global-set-key (kbd "C-x w l") 'visual-line-mode)
-(global-set-key (kbd "M-C-l") 'indent-region)
-(global-set-key (kbd "C-S-<f5>") (lambda () (interactive) (find-file "~/.config/emacs/init.el")))
-(global-set-key (kbd "C-<f5>") (lambda () (interactive) (find-file "~/org/index.org")))
+(defvar my-keys-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (global-set-key (kbd "C-x w c") 'visual-fill-column-mode)
+    (global-set-key (kbd "C-x w l") 'visual-line-mode)
+    (global-set-key (kbd "M-C-l") 'indent-region)
+    (define-key map [?\C-/] 'comment-line)
+    (global-set-key (kbd "S-<f6>") 'lsp-rename)
+    (global-set-key (kbd "C-S-<f5>") (lambda () (interactive) (find-file "~/.config/emacs/init.el")))
+    (global-set-key (kbd "C-<f5>") (lambda () (interactive) (find-file "~/org/index.org")))
+    map)
+  "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  :init-value t
+  :lighter " my-keys")
+
+(my-keys-minor-mode 1)
